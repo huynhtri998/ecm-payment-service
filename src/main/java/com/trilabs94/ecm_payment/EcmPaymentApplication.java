9ac3fcd6db6e56c8909f195f3db5c913f9578fef
huynhtri998 <huynhtri998@gmail.com>
2025-11-30 20:19:26 +0700

    git commit

---
+++ src/main/java/com/trilabs94/ecm_payment/EcmPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.trilabs94.ecm_payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trilabs94/ecm_payment/EcmPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilabs94.ecm_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
